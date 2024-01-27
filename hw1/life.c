ae40e8c73163fcea7c67ad0ddb513bb5d57517bb
BraedenCu <braedencullen120@gmail.com>
2024-01-27 15:35:16 -0500

    functioning: CLI arg handling, field formatting, game_loop + game_loop output formatting. untested: compute_next_gen, compute_next_state. todo: num_living_neighbors

--- hw1/life.c
+++ hw1/life.c
@@ -2,7 +2,7 @@
 Implementations for core functions for Game of Life assignment in CPSC 223 fall 2024
 
 Author: Alan Weide
-Modified by: STUDENT NAME HERE
+Modified by: Braeden Cullen
 Changelog:
  */
 
@@ -15,14 +15,18 @@ bool is_alive(int field[ROWS][COLS], size_t i, size_t j)
     /*
     TODO: Implement this function. This line here only so starter code compiles
     */
-    return false;
+    // ensure that i and j are within the bounds of the field
+    if (i < 0 || i > ROWS || j < 0 || j > COLS) {
+        return false;
+    }
+    return field[i][j] == ALIVE;
 }
 
 unsigned int num_living_neighbors(int field[ROWS][COLS], size_t i, size_t j)
 {
     /*
     TODO: Implement this function. This line here only so starter code compiles
-    */
+    */    
     return 0;
 }
 
@@ -31,6 +35,25 @@ int get_next_state(int field[ROWS][COLS], size_t i, size_t j)
     /*
     TODO: Implement this function. This line here only so starter code compiles
     */
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            if (is_alive(field, i, j)) {
+                if (num_living_neighbors(field, i, j) < 2) {
+                    return DEAD;
+                } else if (num_living_neighbors(field, i, j) > 3) {
+                    return DEAD;
+                } else {
+                    return ALIVE;
+                }
+            } else {
+                if (num_living_neighbors(field, i, j) == 3) {
+                    return ALIVE;
+                } else {
+                    return DEAD;
+                }
+            }
+        }
+    }
     return DEAD;
 }
 
@@ -47,4 +70,9 @@ void compute_next_gen(int cur_field[ROWS][COLS], int next_field[ROWS][COLS])
     /*
     TODO: Implement this function.
      */
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS ; ++j) {
+            next_field[i][j] = get_next_state(cur_field, i, j);
+        }
+    }
 }
--- hw1/main.c
+++ hw1/main.c
@@ -1,7 +1,7 @@
 /*
 Driver for Game of Life assignment in CPSC 223 Fall 2023
 
-Author: [your name here]
+Author: Braeden Cullen
 */
 
 #include <stdlib.h>
@@ -10,6 +10,12 @@ Author: [your name here]
 #include "constants.h"
 #include "life.h"
 #include "helpers.h"
+
+/* 
+ * Game Loop for Game of Life
+ * TODO: Implement neighbors function
+ */
+int game_loop(int time_steps, int field[ROWS][COLS]);
 
 int main(int argc, char *argv[])
 {
@@ -23,23 +29,68 @@ int main(int argc, char *argv[])
      *      a string consisting of the characters '0' and '1', of length ROWS * COLS,
      *      representing the initial state for this run of Game of Life.
      */
+    int time_steps;
+    char *init;
+    time_steps = atoi(argv[1]);
+    init = argv[2];
 
     /*
      * TODO: Create a new statically-allocated array of size ROWS x COLS
      */
+    int field[ROWS][COLS];
+    int i;
+    int j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; ++j) {
+            if (init[i * COLS + j] == '0') { // -'0' is necessary to convert char to int
+                field[i][j] = DEAD;
+            } else {
+                field[i][j] = ALIVE;
+            }
+            //printf("%d", field[i][j]);
+            //printf("\n");
+        }
+    }
 
     /*
      * TODO: Print the initial values held in the field
      */
+    print_initial(field);   
+
+    printf("%d\n", time_steps);
 
     /*
      * TODO: Run the game time_steps times, printing the field at the end of
      * each step
+     * 
+     * game loop
      */
+    game_loop(time_steps, field);
 
     /*
      * Print "Done." then exit.
      */
     printf("Done.\n");
+    return 0;
+}
+
+
+int game_loop(int time_steps, int field[ROWS][COLS]) {
+    int i;
+    int gen;
+    for (i = 0; i < time_steps; ++i) {
+        int next_field[ROWS][COLS];
+        gen = i + 1;
+        compute_next_gen(field, next_field);
+        print_generation(next_field, gen);
+        // copy next_field to field
+        int j;
+        for (j = 0; j < ROWS; ++j) {
+            int k;
+            for (k = 0; k < COLS; ++k) {
+                field[j][k] = next_field[j][k];
+            }
+        }
+    }
     return 0;
 }
