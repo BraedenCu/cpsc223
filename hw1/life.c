9de1fb1fcefc8d17891b49ad9df1bfb245b308f5
BraedenCu <braedencullen120@gmail.com>
2024-01-27 20:15:30 -0500

    code fully functional. still needs reformatting based on guidelines

--- hw1/life.c
+++ hw1/life.c
@@ -4,12 +4,10 @@ Implementations for core functions for Game of Life assignment in CPSC 223 fall 
 Author: Alan Weide
 Modified by: Braeden Cullen
 Changelog:
- */
+*/
 
 #include "life.h"
-
 #include <stdio.h>
-
 #include "helpers.h"
 
 
@@ -17,18 +15,13 @@ bool is_alive(int field[ROWS][COLS], size_t i, size_t j)
 {
     /*
      * TODO: Implement this function. This line here only so starter code compiles
-     *
-     * psuedo code:
-     * check if i and j are within the bounds of the field
-     * if not: return false
-     * check if field[i][j] == ALIVE
-     * if so: return true
-     * else: return false
      * 
      * Reports whether field[i][j] is alive.
      * 
      */
-    if (i < 0 || i >= ROWS || j < 0 || j >= COLS) {
+    // check bounds
+    if (i < 0 || i >= ROWS || j < 0 || j >= COLS) 
+    {
         return false;
     }
     return field[i][j] == ALIVE;
@@ -40,89 +33,70 @@ int get_next_state(int field[ROWS][COLS], size_t i, size_t j)
 {
     /*
      * TODO: Implement this function. This line here only so starter code compiles
-     * psuedo code:
-     * check if should_live
-     * if should_live: return ALIVE
-     * else: return DEAD
-     * 
+     *
      * Reports the state of field[i][j] in the next time step according to the rules of the Game of Life.
      * 
      */
-    int num_neighbors = num_living_neighbors(field, i, j);
+    unsigned int num_neighbors = num_living_neighbors(field, i, j);
 
-    if (is_alive(field, i, j)) { // rule for alive cells
-        if (num_neighbors < 2 || num_neighbors > 3) {
+    if (is_alive(field, i, j)) // rule for alive cells
+    { 
+        if (num_neighbors < 2 || num_neighbors > 3) 
+        {
             return DEAD;
         }
         return ALIVE;
     }
-    else { // rule for dead cells
-        if (num_neighbors == 3) {
+    else 
+    { 
+        if (num_neighbors == 3) // rule for dead cells
+        {
             return ALIVE;
         }
         return DEAD;
     }
 }
 
-
 unsigned int num_living_neighbors(int field[ROWS][COLS], size_t i, size_t j)
 {
     /*
      * TODO: Implement this function. This line here only so starter code compiles
-     * psuedo code
-     * for each neighbor:
-     *    check if neighbor is out of bounds, if so, treat that neighbor as dead
-     *    if neighbor is alive: increment num_living_neighbors
-     * return num_living_neighbors
-     * 
+     *
      * Reports the number of alive neighbors of cell field[i][j].
      * 
      */
-    unsigned int num_living_neighbors = 0;
-    // check top left neighbor
-    if (is_alive(field, i - 1, j - 1)) {
-        num_living_neighbors++;
+    unsigned int    num_living_neighbors = 0;
+    int             row;
+    int             col;
+
+    for (int row_i = -1; row_i <= 1; row_i++) {
+        for (int col_j = -1; col_j <= 1; col_j++) {
+            if (row_i == 0 && col_j == 0) // Skip the cell itself
+            {
+                continue;
+            }
+
+            row = i + row_i;
+            col = j + col_j;
+
+            if (row >= 0 && row < ROWS && col >= 0 && col < COLS) // Check bounds before checking if alive
+            {
+                if (is_alive(field, row, col)) {
+                    num_living_neighbors++;
+                }
+            }
+        }
     }
-    // check top neighbor
-    if (is_alive(field, i - 1, j)) {
-        num_living_neighbors++;
-    }
-    // check top right neighbor
-    if (is_alive(field, i - 1, j + 1)) {
-        num_living_neighbors++;
-    }
-    // check left neighbor
-    if (is_alive(field, i, j - 1)) {
-        num_living_neighbors++;
-    }
-    // check right neighbor
-    if (is_alive(field, i, j + 1)) {
-        num_living_neighbors++;
-    }
-    // check bottom left neighbor
-    if (is_alive(field, i + 1, j - 1)) {
-        num_living_neighbors++;
-    }
-    // check bottom neighbor
-    if (is_alive(field, i + 1, j)) {
-        num_living_neighbors++;
-    }
-    // check bottom right neighbor
-    if (is_alive(field, i + 1, j + 1)) {
-        num_living_neighbors++;
-    }
+
     return num_living_neighbors;
 }
+
 
 void compute_next_gen(int cur_field[ROWS][COLS], int next_field[ROWS][COLS])
 {
     /*
      * TODO: Implement this function.
-     * psuedo code:
-     * for each cell in the field:
-     *     get next state
-     *     set next state in next_field
-     * 
+     *
      * Computes the state at the next time step given the state described by cur_field, storing the next state in next_field.
      */
 
--- hw1/main.c
+++ hw1/main.c
@@ -6,38 +6,63 @@ Author: Braeden Cullen
 
 #include <stdlib.h>
 #include <stdio.h>
-
 #include "constants.h"
 #include "life.h"
 #include "helpers.h"
 
 /* 
- * Game Loop for Game of Life
- * TODO: Implement neighbors function
+ * Game Loop for Conway's Game of Life 
  */
-//int game_loop(int time_steps, int field[ROWS][COLS]);
+void game_loop(int time_steps, int field[ROWS][COLS]);
 
+/* 
+ * Populate the field with the initial values
+ */
+void field_population(int field[ROWS][COLS], char* init);
+
+/* 
+ * Copy the next field to the current field
+ */
+void copy_field(int field[ROWS][COLS], int next_field[ROWS][COLS]);
+
+/*
+ * Main function
+ */ 
 int main(int argc, char *argv[])
 {
-    /*
-     * TODO: Accept two command-line arguments, which will appear in the
-     * following order:
-     * time_steps:
-     *      a positive integer describing how many "generations" the simulation
-     *      will run for.
-     * init:
-     *      a string consisting of the characters '0' and '1', of length ROWS * COLS,
-     *      representing the initial state for this run of Game of Life.
-     */
-    int time_steps;
-    char *init;
+    int     time_steps;
+    char    *init;
+    int     field[ROWS][COLS];
+
     time_steps = atoi(argv[1]);
     init = argv[2];
 
-    /*
-     * TODO: Create a new statically-allocated array of size ROWS x COLS
-     */
-    int field[ROWS][COLS];
+    field_population(field, init);
+
+    print_initial(field);   
+
+    game_loop(time_steps, field);
+    
+    printf("Done.\n");
+    return 0;
+}
+
+void game_loop(int time_steps, int field[ROWS][COLS])
+{
+    int gen;
+    int next_field[ROWS][COLS];
+
+    for (int i = 0; i < time_steps; ++i) 
+    {
+        gen = i + 1;
+        compute_next_gen(field, next_field);
+        print_generation(next_field, gen);
+        copy_field(field, next_field);
+    }
+}
+
+void field_population(int field[ROWS][COLS], char* init)
+{
     for (int i = 0; i < ROWS; i++) 
     {
         for (int j = 0; j < COLS; ++j) 
@@ -46,45 +71,21 @@ int main(int argc, char *argv[])
             {
                 field[i][j] = DEAD;
             } 
-            else {
+            else 
+            {
                 field[i][j] = ALIVE;
             }
         }
     }
-
-    /*
-     * TODO: Print the initial values held in the field
-     */
-    print_initial(field);   
-
-    /*
-     * TODO: Run the game time_steps times, printing the field at the end of
-     * each step
-     * 
-     * game loop
-     */
-    int gen;
-    for (int i = 0; i < time_steps; ++i) 
-    {
-        int next_field[ROWS][COLS];
-        gen = i + 1;
-        compute_next_gen(field, next_field);
-        print_generation(next_field, gen);
-        // copy next_field to field
-        for (int j = 0; j < ROWS; ++j) 
-        {
-            for (int k = 0; k < COLS; ++k) 
-            {
-                field[j][k] = next_field[j][k];
-            }
-        }
-    }
-
-    /*
-     * Print "Done." then exit.
-     */
-    printf("Done.\n");
-    return 0;
 }
 
-
+void copy_field(int field[ROWS][COLS], int next_field[ROWS][COLS])
+{
+    for (int i = 0; i < ROWS; ++i) 
+    {
+        for (int j = 0; j < COLS; ++j) 
+        {
+            field[i][j] = next_field[i][j];
+        }
+    }
+}
