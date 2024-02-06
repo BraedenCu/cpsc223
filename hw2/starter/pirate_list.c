36f3156787a1e52d868c0e3b7e0ea1adbbc7f7e1
BraedenCu <braedencullen120@gmail.com>
2024-02-06 01:13:58 -0500

    hookbook a/b initial commit

---
+++ hw2/starter/pirate_list.c
@@ -0,0 +1,64 @@
+/**
+ * Implementation file for pirate_list for HookBook A in CPSC 223 SP2024.
+ *
+ * Author: [your name here]
+ */
+
+#include "pirate_list.h"
+
+struct pirate_list_instance_t
+{
+    // TODO: Complete this struct definition
+};
+
+pirate_list *list_create()
+{
+    // TODO: Implement this function.
+    // This line is here only so starter code compiles.
+    return NULL;
+}
+
+size_t list_index_of(const pirate_list *pirates, const char *name)
+{
+    // TODO: Implement this function.
+    // This line is here only so starter code compiles.
+    return 0;
+}
+
+pirate *list_insert(pirate_list *pirates, pirate *p, size_t idx)
+{
+    // TODO: Implement this function.
+    // This line is here only so starter code compiles.
+    return NULL;
+}
+
+pirate *list_remove(pirate_list *pirates, const char *name)
+{
+    // TODO: Implement this function.
+    // This line is here only so starter code compiles.
+    return NULL;
+}
+
+const pirate *list_access(const pirate_list *pirates, size_t idx)
+{
+    // TODO: Implement this function.
+    // This line is here only so starter code compiles.
+    return NULL;
+}
+
+void list_sort(pirate_list *pirates)
+{
+    // TODO: Implement this function.
+}
+
+size_t list_length(const pirate_list *pirates)
+{
+    // TODO: Implement this function.
+    // This line is here only so starter code compiles.
+    return 0;
+}
+
+void list_destroy(pirate_list *pirates)
+{
+    // TODO: Implement this function.
+}
