b775e91c1e4cbfa286ca1ca37a9bd9e569db8a54
BraedenCu <braedencullen120@gmail.com>
2024-02-19 22:45:25 -0500

    header files for skills_list

--- hw3/starter/skills_list.c
+++ hw3/starter/skills_list.c
@@ -8,7 +8,18 @@
 
 struct skills_list_instance_t
 {
-    // TODO: Complete this struct definition
+    /**
+     * Consider the following possibilities:
+     * access a particular skill by name?
+     * access a skill at a particular index?
+     * remove a skill from the list?
+     * handle several skills with the same name?
+     * implement a custom type to track additional data about a skill in the list, or will a built-in type suffice?
+     * put the skills in a particular order
+     * print the list of skills?
+     * 
+     * You are required to implement the list of skills as a singly-linked list.
+    */
 };
 
 // TODO: Implement all functions declared in skills_list.h
--- hw3/starter/skills_list.h
+++ hw3/starter/skills_list.h
@@ -5,7 +5,7 @@
  *
  * TO STUDENT: You may add to this file, but do not change anything that is
  *     already here!
- * Author: [your name here]
+ * Author: Braeden
  */
 
 /**
@@ -14,3 +14,60 @@
 typedef struct skills_list_instance_t skills_list;
 
 // TODO: Define and implement an interface for skills_list
+
+/**
+ * Allocates enough memory for a skills_list, and sets the list's length to 0.
+ * 
+ * @return a pointer to a new skills_list with length 0
+*/
+skills_list *list_create();
+
+/**
+ * Appends a skill to the end of the list of skills.
+ * 
+ * @param skills the list of skills to which to append the skill
+ * @param skill the skill to append to the list
+*/
+skills_list list_append(skills_list *skills, char *skill);
+
+/**
+ * Removes the last skill from the list of skills.
+ * 
+ * @param skills the list of skills from which to remove the last skill
+ * @return 0 if the list is empty, 1 if the last skill was removed
+*/
+int list_remove_last(skills_list *skills);
+
+/**
+ * Destroys the list of skills, freeing all memory associated with it.
+ * 
+ * @param skills the list of skills to destroy
+ * @return 0 if the list is empty, 1 if the list was destroyed
+*/
+void list_destroy(skills_list *skills);
+
+/**
+ * Returns the length of the list of skills.
+ * 
+ * @param skills the list of skills to measure
+ * @return the length of the list of skills
+*/
+int list_length(skills_list *skills);
+
+/**
+ * Returns the skill at the given index in the list of skills.
+ * 
+ * @param skills the list of skills from which to get the skill
+ * @param index the index of the skill to get
+ * @return the skill at the given index in the list of skills
+*/
+char *list_get(skills_list *skills, int index);
+
+/**
+ * Returns the index of the given skill in the list of skills.
+ * 
+ * @param skills the list of skills in which to find the skill
+ * @param skill the skill to find in the list
+ * @return the index of the given skill in the list of skills
+*/
+int list_index_of(skills_list *skills, char *skill);
