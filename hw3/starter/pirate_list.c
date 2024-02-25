c4bd39e63ecbd2814e2569b22fc33c7228954c4a
BraedenCu <braedencullen120@gmail.com>
2024-02-25 18:39:09 -0500

    formatting

--- hw3/starter/pirate_list.c
+++ hw3/starter/pirate_list.c
@@ -103,16 +103,6 @@ pirate_list *list_create_with_cmp(compare_fn cmp)
     return lst;
 }
 
-/**
- * Return the index in pirates of the pirate with the name, or a value greater
- *  than or equal to the length of the list if the there is no such pirate.
- *
- * @param pirates the list to search
- * @param name the name to search for
- * @return the index of the pirate with name, or a value greater than or equal
- *  to the length of the list if the there is no such pirate.
- * @assumes neither pirates nor p are NULL
- */
 size_t list_index_of(const pirate_list *pirates, const char *name)
 {
     for (size_t i = 0; i < pirates->list_length; i++) 
@@ -155,21 +145,6 @@ pirate *list_insert(pirate_list *pirates, pirate *p, size_t idx)
     return NULL;
 }
 
-/**
- * Remove the pirate from the list with name, and return a pointer to it.
- *
- * If name is NULL or there is no pirate in the list with name, return NULL.
- *
- * If this function does not return NULL, then the caller owns the returned
- *  pirate.
- *
- * @param pirates the list from which to remove a pirate
- * @param name the name of the pirate to remove
- * @return the removed pirate, or NULL if there is no matching pirate in the
- *  list
- * @assumes pirates is not NULL
- * @assumes every pirate in pirates has a name that is unique in the list
- */
 pirate *list_remove(pirate_list *pirates, const char *name)
 {
     size_t idx = list_index_of(pirates, name);
@@ -196,17 +171,6 @@ pirate *list_remove(pirate_list *pirates, const char *name)
     return removed_pirate;
 }
 
-/**
- * Return a pointer to the pirate pointed to by index idx in the list, or NULL
- *  if idx is not a valid index (i.e., it is >= the length of the list).
- *
- * Ownership of the returned pirate remains with the list.
- *
- * @param pirates the list to access
- * @param idx the index to access of the list
- * @return a pointer to the pirate at index idx of pirates, or NULL
- * @assumes pirates is not NULL
- */
 const pirate *list_access(const pirate_list *pirates, size_t idx)
 {
     if (idx >= pirates->list_length) 
